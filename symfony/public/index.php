8243a64e29cdcbc32ebd8c99e97f3367da68bf9c
Stefan <stefan@stefan-sander.online>
2026-02-20 12:14:05 +0100

    Use APP_ENV in front controller

--- symfony/public/index.php
+++ symfony/public/index.php
@@ -5,7 +5,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-$kernel = new Kernel('dev', true);
+$env = $_SERVER['APP_ENV'] ?? 'dev';
+$debug = isset($_SERVER['APP_DEBUG']) ? (bool) $_SERVER['APP_DEBUG'] : ($env !== 'prod');
+$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
