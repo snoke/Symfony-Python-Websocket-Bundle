94093d9f6f4ae31551843b87c145a2b1b055c813
Stefan <stefan@stefan-sander.online>
2026-02-20 12:26:56 +0100

    Respect APP_ENV from getenv

--- symfony/public/index.php
+++ symfony/public/index.php
@@ -5,8 +5,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-$env = $_SERVER['APP_ENV'] ?? 'dev';
-$debug = isset($_SERVER['APP_DEBUG']) ? (bool) $_SERVER['APP_DEBUG'] : ($env !== 'prod');
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? getenv('APP_ENV') ?: 'dev';
+$debugValue = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? getenv('APP_DEBUG');
+$debug = $debugValue !== false && $debugValue !== null ? (bool) $debugValue : ($env !== 'prod');
 $kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
